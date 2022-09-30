e9e78bd452f46ff99165dbb4729b2977842a488a
Onyebuchi Benedict <84660669+Bengab-coder@users.noreply.github.com>
2022-09-30 12:56:26 +0100

    Task2 completed

--- Task2/client.py
+++ Task2/client.py
@@ -1,8 +1,8 @@
 """
-TASK ONE : MAKING CLIENT RESPOND BACK TO THE SERVER WITH
-			AN ACKNOWLEDGEMENT FROM THE MESSAGE IT RECEIVED FROM THE SERVER
+TASK TWO : CLIENT-SERVER SET INTERSECTION
 """
 import socket
+import random
 from config import (SERVER_HOST,
                     ADDRESS,
                     ENCODE_DECODE_FORMAT,
@@ -13,20 +13,29 @@ from config import (SERVER_HOST,
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect(ADDRESS)
 
+
 # This function will send message to server
-def send(msg):
-	message = msg.encode(ENCODE_DECODE_FORMAT) # Encode message in utf-8 format
-	msg_length = len(message) # Get the length of the message string
-	send_length = str(msg_length).encode(ENCODE_DECODE_FORMAT) # Encode the message
-	send_length += b' ' * (MESSAGE_LENGTH - len(send_length)) # Pad message with spaces to reach length
-	client.send(send_length) # Server expects to know the length of message before actual message
-	client.send(message) # Send the message to the client
+def send(msg, arg=None):
+	message = msg.encode(ENCODE_DECODE_FORMAT)  # Encode message in utf-8 format
+	msg_length = len(message)  # Get the length of the message string
+	send_length = str(msg_length).encode(ENCODE_DECODE_FORMAT)  # Encode the message
+	send_length += b' ' * (MESSAGE_LENGTH - len(send_length))  # Pad message with spaces to reach length
+	client.send(send_length)  # Server expects to know the length of message before actual message
+	client.send(message)  # Send the message to the client
 	received_message = client.recv(MESSAGE_LENGTH).decode(ENCODE_DECODE_FORMAT)  # Decode message to string
 	print("[SERVER]", received_message)
 
 
+random_set = set()
+while len(random_set) < 25:
+	random_set.add(random.randint(1, 100))
+
+
 # Send message to server
-send("Hey Mr server !, It was nice talking to you :) ")
+send(str(random_set))
+
+# Send corrupt package to server since it's not expecting this type
+send("Hahaha lets see how strong you are!")
 
 # Cleanly disconnect client from server
 send(DISCONNECT_MESSAGE)
--- Task2/server.py
+++ Task2/server.py
@@ -1,18 +1,21 @@
 """
-TASK ONE : MAKING CLIENT RESPOND BACK TO THE SERVER WITH
-			AN ACKNOWLEDGEMENT FROM THE MESSAGE IT RECEIVED FROM THE SERVER
+TASK TWO : CLIENT-SERVER SET INTERSECTION
 """
 
+# Importing all necessary modules
 import socket
 import threading
+import random
 
+# Importing constant variables declared in config file
 from config import (SERVER_HOST,
                     ENCODE_DECODE_FORMAT,
                     PORT,
-					ADDRESS,
+                    ADDRESS,
                     MESSAGE_LENGTH,
                     DISCONNECT_MESSAGE)
 
+# Creating a socket object (server) and binding it to the connection characteristics
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind(ADDRESS)
 
@@ -20,27 +23,43 @@ server.bind(ADDRESS)
 # Define function to handle clients separately in another thread
 def handle_client(conn, addr):
 	print(f"New connection from {addr}")
-	conn.send("Thank you for connecting".encode(ENCODE_DECODE_FORMAT))
+	conn.send("Please could you send a set of 25 numbers between 1 and 100?".encode(ENCODE_DECODE_FORMAT))
 	connected = True
 	while connected:
-		msg_length = conn.recv(MESSAGE_LENGTH).decode(ENCODE_DECODE_FORMAT)
+		msg_length = conn.recv(MESSAGE_LENGTH).decode(ENCODE_DECODE_FORMAT)  # Decode the message
 		if msg_length:
 			msg_length = int(msg_length)
 			msg = conn.recv(msg_length).decode(ENCODE_DECODE_FORMAT)
 			print(f"[CLIENT {addr[0]}] {msg}")
-			if msg == DISCONNECT_MESSAGE:
+			if msg == DISCONNECT_MESSAGE: # check for disconnect message to disconnect client cleanly
 				print(f"[DISCONNECT] {addr[0]} has disconnected, bye!")
 				connected = False
-			conn.send("I got your message !".encode(ENCODE_DECODE_FORMAT))
 
+			try: # Error handler for handling errors that may be due to network
+				real_object = eval(msg)
+				# check if client has sent a set object
+				if type(real_object) is set:
+					# Server will now generate its own set of numbers
+					server_set = set()
+					client_set: set = real_object
+					while len(server_set) < len(client_set):
+						server_set.add(random.randint(1, 100))
+					common_values = client_set.intersection(server_set)
+					conn.send(f"Common values are {common_values}".encode(ENCODE_DECODE_FORMAT))
+				else:
+					conn.send("Hey you sent me an invalid type !".encode(ENCODE_DECODE_FORMAT))
+			except Exception as err:
+				conn.send("Got an error, but I handled it anyways".encode(ENCODE_DECODE_FORMAT))
 	conn.close()
 
+
+# Listening for client connections and handling them
 
 server.listen()
 print(f"[RUNNING] Server is listening on {SERVER_HOST}")
 while True:
 	conn, addr = server.accept()
+
+	# Assign function to handle client in new separate thread
 	thread = threading.Thread(target=handle_client, args=(conn, addr))
 	thread.start()
-
-
